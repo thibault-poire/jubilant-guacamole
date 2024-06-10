eeed96839067cf8c07871ccb71a0b3ed72356403
Thibault Poiré <poire.thibault@gmail.com>
2024-06-10 20:56:10 +0200

    feat: project initialization

---
+++ src/lib/components/menu/Menu.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import type { Link } from './Menu.types';
+
+	export let links: Link[];
+</script>
+
+{#if links?.length}
+	<nav aria-label="Main menu">
+		<ul class="flex gap-4">
+			{#each links as { label, url }}
+				<li>
+					<a class="p-2 hover:bg-black hover:text-white" href={url}>{label}</a>
+				</li>
+			{/each}
+		</ul>
+	</nav>
+{/if}
